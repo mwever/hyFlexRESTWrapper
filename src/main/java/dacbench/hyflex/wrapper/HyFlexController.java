e743fc357d14028911dea0654124f69b09cc5774
mwever <wever@mail.uni-paderborn.de>
2021-07-26 13:48:44 +0200

    Fixed issues with HyFlexController and added task to build.gradle for running the HyFlexWrapperApplication spring boot application.

--- src/main/java/dacbench/hyflex/wrapper/HyFlexController.java
+++ src/main/java/dacbench/hyflex/wrapper/HyFlexController.java
@@ -1,5 +1,9 @@
 package dacbench.hyflex.wrapper;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,8 +32,24 @@ public class HyFlexController implements IHyFlexController {
 
 	private static Map<String, ProblemDomain> problemDomainMap = new HashMap<>();
 
-	public HyFlexController() {
-		problemDomainMap.put("42", new SAT(42));
+	private Writer writer;
+
+	private final String fileName = "1episode-" + System.currentTimeMillis() + "-%s.txt";
+	private int counter = 0;
+
+	class TimeDateWriter extends FileWriter {
+		public TimeDateWriter(final File file) throws IOException {
+			super(file);
+		}
+
+		@Override
+		public void write(final String text) throws IOException {
+			super.write(System.currentTimeMillis() + "\t" + text);
+		}
+	}
+
+	public HyFlexController() throws IOException {
+		this.writer = new TimeDateWriter(new File(String.format(this.fileName, this.counter)));
 	}
 
 	private ProblemDomain getProblemDomainOrFail(final String problemDomainID) {
@@ -42,6 +62,12 @@ public class HyFlexController implements IHyFlexController {
 	@PutMapping(value = "/instantiate/{problemType}/{seed}")
 	@Override
 	public String instantiateProblemDomain(@PathVariable("problemType") final String problemType, @PathVariable("seed") final long seed) {
+		try {
+			this.writer.write(String.format("%s %s %s\n", "instantiateProblemDomain", problemType, seed));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		String id = DigestUtils.sha256Hex(System.currentTimeMillis() + problemType + seed + "problemDomain");
 		ProblemDomain pd = null;
 		switch (problemType) {
@@ -76,6 +102,12 @@ public class HyFlexController implements IHyFlexController {
 	@PostMapping("/memorySize/{problemDomainID}/{size}")
 	@Override
 	public void setMemorySize(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("size") final int size) {
+		try {
+			this.writer.write(String.format("%s %s\n", "setMemorySize", size));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		L.debug("Set memory size for problem domain {} to {}", problemDomainID, size);
 		this.getProblemDomainOrFail(problemDomainID).setMemorySize(size);
 	}
@@ -83,6 +115,12 @@ public class HyFlexController implements IHyFlexController {
 	@GetMapping("/toString/{problemDomainID}")
 	@Override
 	public String toString(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s", "toString") + "\n");
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		L.debug("Get string of problem domain {}", problemDomainID);
 		return this.getProblemDomainOrFail(problemDomainID).toString();
 	}
@@ -90,6 +128,12 @@ public class HyFlexController implements IHyFlexController {
 	@GetMapping("/instances/{problemDomainID}")
 	@Override
 	public int getNumberOfInstances(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getNumberOfInstances"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		L.debug("Get number of instances of problem domain {}", problemDomainID);
 		return this.getProblemDomainOrFail(problemDomainID).getNumberOfInstances();
 	}
@@ -97,6 +141,12 @@ public class HyFlexController implements IHyFlexController {
 	@PostMapping("/instance/{problemDomainID}/{instanceID}")
 	@Override
 	public void loadInstance(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("instanceID") final int instanceID) {
+		try {
+			this.writer.write(String.format("%s %s\n", "loadInstance", instanceID));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		L.debug("Load instance {} for problem domain {}.", instanceID, problemDomainID);
 		this.getProblemDomainOrFail(problemDomainID).loadInstance(instanceID);
 	}
@@ -104,60 +154,125 @@ public class HyFlexController implements IHyFlexController {
 	@GetMapping("/search/depth/{problemDomainID}")
 	@Override
 	public double getDepthOfSearch(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getDepthOfsearch"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getDepthOfSearch();
 	}
 
 	@PostMapping("/search/depth/{problemDomainID}/{depthOfSearch}")
 	@Override
 	public void setDepthOfSearch(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("depthOfSearch") final double depthOfSearch) {
+		try {
+			this.writer.write(String.format("%s %s\n", "setdepthOfSearch", depthOfSearch));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		this.getProblemDomainOrFail(problemDomainID).setDepthOfSearch(depthOfSearch);
 	}
 
 	@GetMapping("/mutationIntensity/{problemDomainID}")
 	@Override
 	public double getIntensivityOfMutation(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getIntensitivityOfMutation"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getIntensityOfMutation();
 	}
 
 	@PostMapping("/mutationIntensity/{problemDomainID}/{intensityOfMutation}")
 	@Override
 	public void setIntensityOfMutation(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("intensityOfMutation") final double intensityOfMutation) {
+		try {
+			this.writer.write(String.format("%s %s\n", "setIntensitivityOfMutation", intensityOfMutation));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		this.getProblemDomainOrFail(problemDomainID).setIntensityOfMutation(intensityOfMutation);
 	}
 
 	@GetMapping(value = "/heuristic/record/call/{problemDomainID}")
 	@Override
 	public int[] getHeuristicCallRecord(final @PathVariable("problemDomainID") String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getHeuristicCallRecord"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getHeuristicCallRecord();
 	}
 
 	@GetMapping("/heuristic/record/callTime/{problemDomainID}")
 	@Override
 	public int[] getHeuristicCallTimeRecord(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getHeuristicCallTimeRecord"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getheuristicCallTimeRecord();
 	}
 
 	@GetMapping("/heuristic/{problemDomainID}/{heuristicType}")
 	@Override
 	public int[] getHeuristicsOfType(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("heuristicType") final String heuristicType) {
-		return this.getProblemDomainOrFail(problemDomainID).getHeuristicsOfType(HeuristicType.valueOf(heuristicType));
+		try {
+			this.writer.write(String.format("%s %s\n", "getHeuristicOfType", heuristicType));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int[] heuristicsOfType = this.getProblemDomainOrFail(problemDomainID).getHeuristicsOfType(HeuristicType.valueOf(heuristicType));
+		if (heuristicsOfType == null) {
+			return new int[] {};
+		} else {
+			return heuristicsOfType;
+		}
 	}
 
 	@GetMapping("/heuristic/mutationIntensity/{problemDomainID}")
 	@Override
 	public int[] getHeuristicsThatUseIntensityOfMutation(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getHeuristicsThatUseIntensityOfMutation"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getHeuristicsThatUseIntensityOfMutation();
 	}
 
 	@GetMapping("/heuristic/depth/{problemDomainID}")
 	@Override
 	public int[] getHeuristicsThatUseDepthOfSearch(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getHeuristicsThatUseDepthOfSearch"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getHeuristicsThatUseDepthOfSearch();
 	}
 
 	@GetMapping("/heuristic/num/{problemDomainID}")
 	@Override
 	public int getNumberOfHeuristics(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s\n", "getNumberOfHeuristics"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getNumberOfHeuristics();
 	}
 
@@ -165,6 +280,12 @@ public class HyFlexController implements IHyFlexController {
 	@Override
 	public double applyHeuristic(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("heuristicID") final int heuristicID, @PathVariable("solutionSourceIndex") final int solutionSourceIndex,
 			@PathVariable("solutionDestinationIndex") final int solutionDestinationIndex) {
+		try {
+			this.writer.write(String.format("%s %s %s %s\n", "applyHeuristic", heuristicID, solutionSourceIndex, solutionDestinationIndex));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).applyHeuristic(heuristicID, solutionSourceIndex, solutionDestinationIndex);
 	}
 
@@ -172,49 +293,105 @@ public class HyFlexController implements IHyFlexController {
 	@Override
 	public double applyHeuristic2(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("heuristicID") final int heuristicID, @PathVariable("solutionSourceIndex1") final int solutionSourceIndex1,
 			@PathVariable("solutionSourceIndex2") final int solutionSourceIndex2, @PathVariable("solutionDestinationIndex") final int solutionDestinationIndex) {
+		try {
+			this.writer.write(String.format("%s %s %s %s %s\n", "applyHeuristic", heuristicID, solutionSourceIndex1, solutionSourceIndex2, solutionDestinationIndex));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).applyHeuristic(heuristicID, solutionSourceIndex1, solutionSourceIndex2, solutionDestinationIndex);
 	}
 
 	@PutMapping("/solution/init/{problemDomainID}/{index}")
 	@Override
 	public void initialiseSolution(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("index") final int index) {
+		try {
+			this.writer.write(String.format("%s %s\n", "initialiseSolution", index));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		this.getProblemDomainOrFail(problemDomainID).initialiseSolution(index);
 	}
 
 	@GetMapping("/solution/best/toString/{problemDomainID}")
 	@Override
 	public String getBestSolutionToString(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s %s\n", "getBestSolutionToString"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).bestSolutionToString();
 	}
 
 	@GetMapping("/solution/best/value/{problemDomainID}")
 	@Override
 	public double getBestSolutionValue(@PathVariable("problemDomainID") final String problemDomainID) {
+		try {
+			this.writer.write(String.format("%s %s\n", "getBestSolutionValue"));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getBestSolutionValue();
 	}
 
 	@PostMapping("/solution/copy/{problemDomainID}/{solutionSourceIndex}/{solutionDestinationIndex}")
 	@Override
 	public void copySolution(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("solutionSourceIndex") final int solutionSourceIndex, @PathVariable("solutionDestinationIndex") final int solutionDestinationIndex) {
+		try {
+			this.writer.write(String.format("%s %s %s\n", "copySolution", solutionSourceIndex, solutionDestinationIndex));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		this.getProblemDomainOrFail(problemDomainID).copySolution(solutionSourceIndex, solutionDestinationIndex);
 	}
 
 	@GetMapping("/solution/toString/{problemDomainID}/{solutionIndex}")
 	@Override
 	public String getSolutionToString(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("solutionIndex") final int solutionIndex) {
+		try {
+			this.writer.write(String.format("%s %s\n", "getSolutionToString", solutionIndex));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).solutionToString(solutionIndex);
 	}
 
 	@GetMapping("/solution/functionValue/{problemDomainID}/{solutionIndex}")
 	@Override
 	public double getFunctionValue(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("solutionIndex") final int solutionIndex) {
+		try {
+			this.writer.write(String.format("%s %s\n", "getFunctionValue", solutionIndex));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).getFunctionValue(solutionIndex);
 	}
 
 	@GetMapping("/solution/compare/{problemDomainID}/{solutionIndex1}/{solutionIndex2}")
 	@Override
 	public boolean compareSolutions(@PathVariable("problemDomainID") final String problemDomainID, @PathVariable("solutionIndex1") final int solutionIndex1, @PathVariable("solutionIndex2") final int solutionIndex2) {
+		try {
+			this.writer.write(String.format("%s %s\n", "compareSolutions", solutionIndex1, solutionIndex2));
+			this.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return this.getProblemDomainOrFail(problemDomainID).compareSolutions(solutionIndex1, solutionIndex2);
+	}
+
+	@GetMapping("/changelog")
+	public void changeLogFile() throws IOException {
+		this.counter++;
+		this.writer.flush();
+		this.writer.close();
+		this.writer = new TimeDateWriter(new File(String.format(this.fileName, this.counter)));
 	}
 
 }
